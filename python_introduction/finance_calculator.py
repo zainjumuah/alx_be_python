fccbe66ab20921145773621eec1f2f7e97f92a52
zainjumuah <your.email@example.com~>
2025-11-09 18:17:07 +0100

    remember to hide this repo zain

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-user_monthly_income = (int)(input("Enter your monthly income: "))
-user_monthly_expenses = (int)(input("Enter your total monthly expenses: "))
+user_monthly_income = (float)(input("Enter your monthly income: "))
+user_monthly_expenses = (float)(input("Enter your total monthly expenses: "))
 monthly_savings = user_monthly_income  -  user_monthly_expenses
-projected_savings = monthly_savings * 12  + (monthly_savings * 12  * 0.05)
+projected_savings = (float)(monthly_savings * 12  + (monthly_savings * 12  * 0.05))
 print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is ${projected_savings}.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,3 +1,3 @@
 hours = 2
-seconds = 3600 * 2
+seconds = 3600 * hours
 print(f"2 hour(s) is {seconds} seconds.")
