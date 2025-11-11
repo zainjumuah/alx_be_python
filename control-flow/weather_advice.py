a0de2eb9a378c4554f27b877d1ea7b0402384d48
zainjumuah <your.email@example.com~>
2025-11-11 13:28:04 +0100

    if-elif-else instead of match-case

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,10 @@
 current_weather = input("What's the weather like today? (sunny/rainy/cold):")
 
-match current_weather:
-    case "sunny":
-        print("Wear a t-shirt and sunglasses.")
-    case "rainy":
-        print("Don't forget your umbrella and a raincoat.")
-    case "cold":
-        print("Make sure to wear a warm coat and a scarf.")
-    case _:
-        print("Sorry, I don't have recommendations for this weather.")
+if current_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif current_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif current_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
