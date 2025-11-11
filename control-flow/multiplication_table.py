e46abc682c3631193873e3e0cf09833315d6c3a6
zainjumuah <your.email@example.com~>
2025-11-11 13:43:27 +0100

    variable_name fixes to get paast the checker

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 # Looping from 1 through to 10'
-for s in range(1, 11, 1):
+for s in range(1, 11):
     print(f"{number} * {s} = {number * s}")
